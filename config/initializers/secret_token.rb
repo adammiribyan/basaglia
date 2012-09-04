5ce2890b9bde2165362800822bd684c67ae8c596
Adam Miribyan <adam.miribyan@gmail.com>
2012-09-04 04:03:42 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basaglia::Application.config.secret_token = '1fffa143b5851cb854ed3f0b3605cbca8222d55c03fb5eb6030b6c68c8ba27c7a225caf3b21b94bb14ee85397bacfb598e7b1389645c5ec50b871fbaa080dac6'
