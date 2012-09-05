e647c515ad95ab03b777205f075a7065f15f811d
Adam Miribyan <adam.miribyan@gmail.com>
2012-09-05 21:22:59 +0200

    Pages controller with www.basaglia.com/trainings like routes.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+  VALID_CHARACTERS = "a-zA-Z0-9~!@$%^&*()#`_+-=<>\"{}|[];',?".freeze
+
+  rescue_from ActionView::MissingTemplate do |exception|
+    if exception.message =~ %r{Missing template #{content_path}}
+      raise ActionController::RoutingError, "No such page: #{params[:id]}"
+    else
+      raise exception
+    end
+  end
+
+  def show
+    render template: current_page
+  end
+
+  protected
+
+    def current_page
+      "#{content_path}#{clean_path}"
+    end
+
+    def clean_path
+      path = Pathname.new("/#{clean_id}")
+      path.cleanpath.to_s[1..-1]
+    end
+
+    def content_path
+      "pages/"
+    end
+
+    def clean_id
+      params[:id].tr("^#{VALID_CHARACTERS}", '')
+    end
+end
